b6e04ea2f2ba56bb4c29fb5118f3b63afa7aebb5
Yandiswa <ypmakanda@gmail.com>
2021-08-17 12:56:25 +0200

    FIX: naming convention

--- task-0-5.js
+++ task-0-5.js
@@ -1,5 +1,5 @@
-function areaOfaTriangle(base,hight,length){
-    var side = (base + hight + length) / 2;
-    return Math.sqrt(side * (side - base) * (side - hight) * (side - length));
+function areaOfaTriangle(s1,s2,s3){
+    var s = (s1 + s2 + s3) / 2;
+    return Math.sqrt(s * (s - s1) * (s - s2) * (s - s3));
 }
 areaOfaTriangle(3,4,5);
