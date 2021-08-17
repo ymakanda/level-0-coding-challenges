7e15fb12f260469eddf9d4f23698070e222f90d1
Yandiswa <ypmakanda@gmail.com>
2021-08-17 13:00:56 +0200

    Fix: handling  singulars and plurals for both hours and minutes

--- task-0-8.js
+++ task-0-8.js
@@ -1,7 +1,10 @@
 function convertAnyNumberIntoHoursAndMinutes(num){
-    var hours = Math.floor(num / 60);  
-    var minutes = num % 60;
-    return hours + " hours, " + minutes + " minutes"; 
+    var hour = Math.floor(num / 60);  
+    var minute = num % 60;
+    var totalHours = (hour <= 1) ?  hour + " hour, " : hour + " hours, "
+    var totalMinutes = (minute <= 1) ?  minute + " minute" : minute + " minutes"
+
+    return totalHours + totalMinutes;
 }
 console.log(convertAnyNumberIntoHoursAndMinutes(71));
 console.log(convertAnyNumberIntoHoursAndMinutes(133));
