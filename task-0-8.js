443685c4c35716458aaf648695a732070c53c80b
Yandiswa <ypmakanda@gmail.com>
2021-08-18 11:23:12 +0200

    Updated singulars and plurals for times

--- task-0-8.js
+++ task-0-8.js
@@ -1,10 +1,8 @@
 function convertAnyNumberIntoHoursAndMinutes(num){
     var hour = Math.floor(num / 60);  
     var minute = num % 60;
-    var totalHours = (hour <= 1) ?  hour + " hour, " : hour + " hours, "
-    var totalMinutes = (minute <= 1) ?  minute + " minute" : minute + " minutes"
-
-    return totalHours + totalMinutes;
+    return (hour <= 1) ?  hour + " hour, " + minute + " minutes" 
+        : hour + " hours, " + minute + " minutes";
 }
-console.log(convertAnyNumberIntoHoursAndMinutes(71));
+console.log(convertAnyNumberIntoHoursAndMinutes(60));
 console.log(convertAnyNumberIntoHoursAndMinutes(133));
