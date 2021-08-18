56859a006e290cc13a5ab91e6e82fc64e7a29b5e
Yandiswa <ypmakanda@gmail.com>
2021-08-18 11:26:02 +0200

    remove duplicate vowels

--- task-0-9.js
+++ task-0-9.js
@@ -1,9 +1,18 @@
-function getVowelsInTheString(strg){
- 
-    let starRegex = /[aeiou]/ig; 
-    let result = strg.match(starRegex);
+function getVowelsInTheString(str){
 
-    console.log("vowels in the string: " + result);
+   var str = str.toLowerCase(str.split(''));
+   var vowels = 'aeiou'.split('');
+   var commonVowels = [];
+   var i, j;
+   for (i = 0; i < str.length; i++) {
+      for (j = 0; j < vowels.length; j++) {
+         if(str[i] === vowels[j]){
+            if (commonVowels.indexOf(vowels[j]) < 0){
+                commonVowels.push(vowels[j])
+            }
+         }
+      }
+   }
+   console.log("Vowels: " + commonVowels);
 }
-console.log(getVowelsInTheString("Umuzi"));
-
+getVowelsInTheString("Umuzi");
