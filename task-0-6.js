dbe086b8c6e6c161a681a9cf52f594d8c55e3055
Yandiswa <ypmakanda@gmail.com>
2021-08-20 09:47:53 +0200

    Fix: getMaxNumber for both negative and positive

--- task-0-6.js
+++ task-0-6.js
@@ -1,18 +1,17 @@
  function getMaximumNumber(){
 
     var numeric_array = [];
-    var max = 0;
+    var max = arguments[0];
     var i;
 
     for(var i = 0; i < arguments.length; i++){
       numeric_array.push( arguments[i]);
     }
-    for (i = 0; numeric_array.length > i; i++) {
-
+    for (i = 0; i < numeric_array.length ; i++) {
       if (numeric_array[i] > max) {
           max = numeric_array[i];
       }
     }
     return max;
 }
-getMaximumNumber(90,30,4,10);
+getMaximumNumber(-90, -30, -4, -10);
