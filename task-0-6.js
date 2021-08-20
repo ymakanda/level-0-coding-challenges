80d514d0b28573d089b212a77ab6f45061e16981
Yandiswa <ypmakanda@gmail.com>
2021-08-20 15:59:31 +0200

    updated var to use camel casing

--- task-0-6.js
+++ task-0-6.js
@@ -1,17 +1,17 @@
  function getMaxNumber(){
 
-    var numeric_array = [];
+    var numericArray = [];
     var max = arguments[0];
     var i;
 
     for(var i = 0; i < arguments.length; i++){
-      numeric_array.push( arguments[i]);
+      numericArray.push( arguments[i]);
     }
-    for (i = 0; i < numeric_array.length ; i++) {
-      if (numeric_array[i] > max) {
-          max = numeric_array[i];
+    for (i = 0; i < numericArray.length ; i++) {
+      if (numericArray[i] > max) {
+          max = numericArray[i];
       }
     }
     return max;
 }
-getMaximumNumber(-90, -30, -4, -10);
+getMaxNumber(-90, -30, -4, -10);
