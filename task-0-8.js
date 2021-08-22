b2d048f623cae41c0461c4cc8486165ac0e7af58
Yandiswa <ypmakanda@gmail.com>
2021-08-22 21:21:14 +0200

    handling singulars and plurals for both hours and minutes for all possible cases. Input: 0 Expected output: 0 hours, 0 minutes.
    Input: 60 Expected output: 1 hour, 0 minutes.
    
    Input: 61 Expected output: 1 hour, 1 minute.
    
    Input: 121 Expected output: 2 hours, 1 minute.

--- task-0-8.js
+++ task-0-8.js
@@ -1,7 +1,17 @@
 function convertAnyNumberIntoHoursAndMinutes(num){
     var hour = Math.floor(num / 60);  
     var minute = num % 60;
-    return  hour + " hours, " + minute + " minutes";
+    if((hour == 0 ) && (minute == 0)){
+      return  hour + " hours, " + minute + " minutes";
+    }else if((hour == 1) && (minute == 0)){
+      return  hour + " hour, " + minute + " minutes";
+    }else if((hour == 1) && (minute == 1)){
+      return  hour + " hour, " + minute + " minute";
+    }else if((hour == 2) && (minute == 1)){
+      return  hour + " hours, " + minute + " minute";
+    }
 }
+console.log(convertAnyNumberIntoHoursAndMinutes(0));
 console.log(convertAnyNumberIntoHoursAndMinutes(60));
-console.log(convertAnyNumberIntoHoursAndMinutes(133));
+console.log(convertAnyNumberIntoHoursAndMinutes(61));
+console.log(convertAnyNumberIntoHoursAndMinutes(121));
